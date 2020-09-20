2e2c3a8e5272b93e492dd78fb38f57d29bc1c7e8
Jorge Lopez <jorgelopezseijas@gmail.com>
2020-09-20 09:32:22 +0200

    Change wrapper in order to include self and user_id param

--- src/util/database.py
+++ src/util/database.py
@@ -5,16 +5,14 @@ from src.util.dto import DTOError
 from src.model.model import Users
 from src.util.http_codes import Status
 
-
 def requires_user(f):
     @wraps(f)
-    def wrap(*args, **kwargs):
-        user_id = kwargs['user_id']
+    def wrap(self, user_id, *args, **kwargs):
         try:
             user = Users.get(Users.id_user == user_id)
         except Users.DoesNotExist:
             return DTOError(status_code=Status.HTTP_404_NOT_FOUND,
                             message="User not found!",
                             code="not_found").to_response()
-        return f(*args, user)
+        return f(self, user, *args, **kwargs)
     return wrap
