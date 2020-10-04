e1422612553a0911fcadd03d39ead6ff16563c79
Jorge Lopez <jorgelopezseijas@gmail.com>
2020-10-04 19:29:23 +0200

    Added authentication decorator and modify AccessToken class

--- src/util/authentication.py
+++ src/util/authentication.py
@@ -1,24 +1,30 @@
 import os
 import jwt
-from datetime import datetime, timedelta
 
+from datetime import datetime, timedelta
+from flask import request
+from functools import wraps
+from src.model.model import Tokens
+from src.util.dto import DTOError
+from src.util.http_codes import Status
+request
 
 class AccessToken:
 
-    def __init__(self, user_id):
-        self.__user_id = user_id
+    def __init__(self):
         self.__secret_token = os.getenv('SECRET_TOKEN', 'shhhh!!')
-        self.__exp_delta = os.getenv('TOKEN_EXP_DELTA', 10*60*60)
+        self.__exp_delta = os.getenv('TOKEN_EXP_DELTA', 60*60)
         self.__token_algorithm = os.getenv('TOKEN_ALGORITHM', 'HS256')
 
-    def generate(self):
+    def generate(self, user_id):
         payload = {
-            'user_id': self.__user_id,
+            'user_id': user_id,
             'exp': datetime.utcnow() + timedelta(seconds=self.__exp_delta)
         }
 
         return jwt.encode(
-            payload, self.__secret_token, self.__token_algorithm).decode('utf-8')
+                        payload, self.__secret_token,
+                        self.__token_algorithm).decode('utf-8')
 
     def validate(self, token):
         if token:
@@ -31,5 +37,35 @@ class AccessToken:
 
         return False, None
 
-    def update(self):
-        pass
+    def update(self, token):
+        result, payload = self.validate(token)
+
+        if(not result):
+            return False, None
+
+        return True, self.generate(payload['user_id'])
+
+
+def requires_auth(f):
+    @wraps(f)
+    def wrap(self, *args, **kwargs):
+        access_token = AccessToken()
+        token = request.headers.get('Authorization', None)
+        result, payload = access_token.validate(token)
+
+        if result:
+            try:
+                user_id = payload.get('user_id')
+                token = Tokens.get(
+                                token=token,
+                                id_user=user_id)
+            except Tokens.DoesNotExist:
+                result = False
+
+        if not result:
+            return DTOError(status_code=Status.HTTP_401_UNAUTHORIZED,
+                            message="User not authenticated",
+                            code="unhautorized").to_response()
+
+        return f(self, *args, **kwargs)
+    return wrap
